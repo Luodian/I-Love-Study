50ab9bd65620a61858fda4d21c2eade29ee5b6bc
Luodian <1121058986@qq.com>
2017-08-10 16:03:18 +0800

    二维码页面，图标更新
    现在它有了一个太阳图标，二维码页面的menu改成了photo，但是还没做导航。

--- I-love-study/app/src/main/java/com/example/luodian/i_love_study/MainActivity.java
+++ I-love-study/app/src/main/java/com/example/luodian/i_love_study/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.luodian.i_love_study;
 
+import android.Manifest;
 import android.app.ActionBar;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,14 +19,16 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button QRbutton = null;
+    private Toolbar myToolBar = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         QRbutton = (Button) this.findViewById(R.id.qr_button);
         QRbutton.setOnClickListener(this);
-        Toolbar myToolBar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolBar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolBar);
+
     }
 
     @Override
@@ -30,9 +36,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         if (view == QRbutton)
         {
-            Intent intent = new Intent();
-            intent.setClass(MainActivity.this,QRActivity.class);
-            MainActivity.this.startActivity(intent);
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},60);
+            }
+            else
+            {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,QRActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
         }
     }
 
@@ -41,6 +54,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.tool_bar,menu);
+        myToolBar.getMenu().findItem(R.id.tb_settings).setVisible(true);
+        myToolBar.getMenu().findItem(R.id.tb_photos).setVisible(false);
         return true;
     }
 
--- I-love-study/app/src/main/java/com/example/luodian/i_love_study/QRActivity.java
+++ I-love-study/app/src/main/java/com/example/luodian/i_love_study/QRActivity.java
@@ -2,6 +2,12 @@ package com.example.luodian.i_love_study;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,22 +20,45 @@ import android.widget.Toast;
 import com.google.zxing.Result;
 import com.google.zxing.client.result.ParsedResult;
 import com.google.zxing.client.result.ParsedResultType;
+import com.mylhyl.zxing.scanner.OnScannerCompletionListener;
 import com.mylhyl.zxing.scanner.ScannerView;
+import com.mylhyl.zxing.scanner.common.Scanner;
 
-public class QRActivity extends AppCompatActivity
-{
-    private ScannerView mScannerView = null;
+public class QRActivity extends AppCompatActivity implements OnScannerCompletionListener {
+    public static final String EXTRA_LASER_LINE_MODE = "extra_laser_line_mode";
+    public static final String EXTRA_SCAN_MODE = "extra_scan_mode";
+    private ScannerView mScannerView;
+    private Result mLastResult;
+    private int laserMode;
+    private boolean scanMode = true;
+
+    private Toolbar myToolBar = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
-        Toolbar myToolBar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolBar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolBar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
         mScannerView = (ScannerView) findViewById(R.id.qr_scanner_view);
+        mScannerView.setOnScannerCompletionListener(this);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+        {
+            laserMode = extras.getInt(EXTRA_LASER_LINE_MODE);
+            scanMode = extras.getBoolean(EXTRA_SCAN_MODE);
+        }
+
+        mScannerView.setMediaResId(R.raw.beep);
+        mScannerView.setDrawText("将二维码放入框内",true);
+        mScannerView.setDrawTextColor(Color.WHITE);
+        mScannerView.setLaserGridLineResId(R.mipmap.grid_scan_line);
+        mScannerView.setLaserFrameBoundColor(0xFF26CEFF);
     }
 
     @Override
@@ -37,6 +66,8 @@ public class QRActivity extends AppCompatActivity
     {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.tool_bar,menu);
+        myToolBar.getMenu().findItem(R.id.tb_settings).setVisible(false);
+        myToolBar.getMenu().findItem(R.id.tb_photos).setVisible(true);
         return true;
     }
 
@@ -44,9 +75,9 @@ public class QRActivity extends AppCompatActivity
     public boolean onOptionsItemSelected(MenuItem item)
     {
         int itemID = item.getItemId();
-        if (itemID == R.id.tb_settings)
+        if (itemID == R.id.tb_photos)
         {
-            Toast.makeText(this,"Settings",0).show();
+            Toast.makeText(this,"Photos",0).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -66,9 +97,18 @@ public class QRActivity extends AppCompatActivity
         super.onPause();
     }
 
-    void onScannerCompletion(Result rawResult, ParsedResult parsedResult, Bitmap barcode)
+    private void restartPreviewAfterDelay(long delayMS) {
+        mScannerView.restartPreviewAfterDelay(delayMS);
+        resetStatusView();
+    }
+
+    private void resetStatusView() {
+        mLastResult = null;
+    }
+
+    @Override
+    public void OnScannerCompletion(Result rawResult, ParsedResult parsedResult, Bitmap barcode)
     {
         ParsedResultType type = parsedResult.getType();
     }
-
 }
